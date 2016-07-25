c4c9075ef41ab801449f84976657a4a4ab5ddecd
Dima Knivets <knivets@gmail.com>
2016-07-25 18:41:27 +0300

    xls formatting

--- app/controllers/statements_controller.rb
+++ app/controllers/statements_controller.rb
@@ -121,65 +121,74 @@ class StatementsController < ApplicationController
     sheet = book.create_worksheet
     bold = Spreadsheet::Format.new weight: :bold
     s = @statement
+    length = 0
+    summary = [
+      ['Agent', s.user.name],
+      ['Statement Month', s.date.strftime('%B %Y')],
+      ['', ''],
+      ['', ''],
+      ['Total Agent Commissions', "$#{s.agent_com}"],
+      ['', ''],
+      ['Commissions by Carrier', "$#{s.carrier_com}"],
+    ]
+    z = s.coms_by_carrier.each { |car, com| summary << [car, com]}
+    policies = records
+      .map(&:client)
+      .map(&:policy)
+      .map {|x| [x.id, x.carrier] }
+    policies.uniq! {|x| x.first }
+    n_sum = []
+    #sheet.row(1).format(1).border = :thin
+    t = 0
+    summary.each_with_index do |row, i|
+      sheet.row(length + i).replace (row)
+      sheet.row(length + i).set_format(0, (Spreadsheet::Format.new :weight => :bold))
+      t += 1
+    end
+    length += (t+2)
     cols = [
       'ID',
-      'Agent',
-      'Carrier',
       'Client',
       'Policy',
       'Statement Month',
       'Earned Month',
     ]
-    sheet.row(0).replace cols
-    sheet.row(0).default_format = bold
-    length = 0
-    records.each_with_index do |record, i|
-      c = []
-      c << record.id
-      if record.client.user
-        c << record.client.user.name 
-      else
-        c << ''
-      end
-      if record.client.policy
-        c << record.client.policy.carrier
-      else
-        c << ''
-      end
-      if record.client
-        c << record.client.name 
-      else
-        c << ''
-      end
-      if record.client.policy
-        c << record.client.policy.kind
-      else
-        c << ''
-      end
-      c << record.statement_date.strftime('%m-%d-%Y')
-      c << record.earned_date.strftime('%m-%d-%Y')
-      sheet.row(1 + i).replace c
+    length += 1
+    policies.each do |policy|
+      sheet.row(length).set_format(0, bold)
+      sheet.row(length).replace [policy.second, "Total: #{z[policy.second]}"]
       length += 1
+      sheet.row(length).replace cols
+      sheet.row(length).default_format = bold
+      length += 1
+      t = 0
+      cls = Client.where(policy_id: policy.first).map(&:id)
+      records.where(client_id: cls).each_with_index do |record, i|
+        c = []
+        c << record.id
+        if record.client
+          c << record.client.name 
+        else
+          c << ''
+        end
+        if record.client.policy
+          c << record.client.policy.kind
+        else
+          c << ''
+        end
+        c << record.statement_date.strftime('%m-%d-%Y')
+        c << record.earned_date.strftime('%m-%d-%Y')
+        sheet.row(length + i).replace c
+        t += 1
+      end
+      length += (t+2)
     end
     len = [10, 20, 20, 20, 20, 20]
     len.each_with_index do |col, i|
       sheet.column(i).width = col
     end
     length += 4
-    summary = [
-      ['Agent', s.user.name],
-      ['Statement Month', s.date.strftime('%m-%d-%Y')],
-      ['Total Commissions Received', s.total_com],
-      ['Total Agent Commissions', s.agent_com],
-      ['Commissions by Carrier', s.carrier_com],
-    ]
-    s.coms_by_carrier.each { |car, com| summary << [car, com]}
-    n_sum = []
-    summary.each_with_index do |row, i|
-      z = [''] * 5
-      sheet.row(length + i).replace (z + row)
-      sheet.row(length + i).set_format(5, (Spreadsheet::Format.new :weight => :bold))
-    end
+    
     code = SecureRandom.hex 
     file = "#{Rails.root}/public/tmp/files/#{code}.xls"
     File.open(file, "w") {}
